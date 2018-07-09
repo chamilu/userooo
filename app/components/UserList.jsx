8ddf789c170346739ae3aab655e0945a1866aa92
Chamil Udayanga <chamiludayanga28@gmail.com>
2018-07-09 22:42:10 +0530

    added get users functionality

--- app/components/UserList.jsx
+++ app/components/UserList.jsx
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import User from './User';
 
 export default class UserList extends Component {
+  getUsers() {
+    return axios.get('/api/users');
+  }
+
+  componentDidMount() {
+    this.getUsers()
+      .then(data => {
+        console.log(data);
+      })
+      .catch(console.error);
+  }
+
   render() {
     return <User />;
   }
--- server.js
+++ server.js
@@ -1,25 +1,33 @@
 require('dotenv').config();
 const fastify = require('fastify')();
+const path = require('path');
 const cors = require('cors');
 const { getConnectUrl } = require('./utils/db');
 
-// fastify.use(cors());
+const { COLLECTION_USERS } = require('./constants');
+const port = process.env.PORT || 3000;
+
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'dist'),
+});
 
 fastify.register(require('fastify-mongodb'), {
   url: getConnectUrl(),
   useNewUrlParser: true,
 });
 
-fastify.get('/status', async (request, reply) => {
-  reply.type('application/json').code(200);
-  return true;
+fastify.get('/api/users', async (request, reply) => {
+  const db = fastify.mongo.db;
+
+  const users = await db
+    .collection(COLLECTION_USERS)
+    .find({})
+    .toArray();
+
+  reply.type('application/json').send(users);
 });
 
-fastify.get('/', (request, reply) => {
-  reply.send({ hello: 'world' });
-});
-
-fastify.listen(3000, err => {
+fastify.listen(port, err => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
